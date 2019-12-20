74c08b2b5ef7f869e3502279949064761254918e
zxhRMCX <2583111340@qq.com>
2019-12-20 09:32:22 +0800

    src

--- src/main/java/com/example/demo3/dao/AdminUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo3.dao;
-
-import com.example.demo3.entity.AdminUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AdminUserDao extends JpaRepository<AdminUser, Integer> {
-    AdminUser findByUsernameAndPassword(String username, String pwd);
-}
